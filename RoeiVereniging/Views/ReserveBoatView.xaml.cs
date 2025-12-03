61df22cdede4bc48f82f7f558f3180677ba5fe81
s1210724 <s1210724@student.windesheim.nl>
2025-12-03 16:39:13 +0100

    feat(wip): changed selectionlogic so a enum can be selected.

--- RoeiVereniging/Views/ReserveBoatView.xaml.cs
+++ RoeiVereniging/Views/ReserveBoatView.xaml.cs
@@ -14,15 +14,15 @@ public partial class ReserveBoatView : ContentPage
         _viewModel = viewModel;
     }
 
-    private void TypePicker_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
+    //private void TypePicker_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    var picker = (Picker)sender;
+    //    int selectedIndex = picker.SelectedIndex;
 
-        if (selectedIndex != -1)
-        {
-            BoatType Type = (BoatType)picker.SelectedItem;
-            _viewModel.NewSelectedBoat(Type);
-        }
-    }
+    //    if (selectedIndex != -1)
+    //    {
+    //        BoatType Type = (BoatType)picker.SelectedItem;
+    //        _viewModel.NewSelectedBoat(Type);
+    //    }
+    //}
 }
