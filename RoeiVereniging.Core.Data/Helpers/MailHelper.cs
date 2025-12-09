62b6c04375c54973d87dc42fe3f8dcdbc56c18ae
s1210724 <s1210724@student.windesheim.nl>
2025-12-09 21:38:41 +0100

    feat: made sendmail function asyncronous so loading times improve and the app feels smoother.

--- RoeiVereniging.Core.Data/Helpers/MailHelper.cs
+++ RoeiVereniging.Core.Data/Helpers/MailHelper.cs
@@ -12,12 +12,12 @@ namespace RoeiVereniging.Core.Data.Helpers
 {
     public class MailHelper
     {
-        public void SendMail(string userEmail, string subject, string message)
+        public async Task SendMail(string userEmail, string subject, string message)
         {
             var styledMessage = $"<div style=\"width: 100%; align-content: center; justify-content:center; background-color:#5FA6E8;\">\r\n <h1 style=\"text-align: center; margin: auto; width: fit-content; font-weight: bold; font-size: 5em; color: red;\">{subject}</h1>" +
                 $"\r\n <p style=\"text-align: center; margin: 50px auto; padding: 1rem; border-radius: 15px; color: white; width: fit-content; font-size: 3em; font-weight:900; background-color: #082757\">{message}</p>\r\n</div>";
 
-            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            using var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
             {
                 Credentials = new NetworkCredential("d62aaaf8aa2dc2", "3033c5ff558831"),
                 EnableSsl = true
@@ -33,8 +33,9 @@ namespace RoeiVereniging.Core.Data.Helpers
 
             mailMessage.To.Add(userEmail);
 
-            client.Send(mailMessage);
-            System.Console.WriteLine("Sent");
+            await client.SendMailAsync(mailMessage);
+
+            Console.WriteLine("Sent");
         }
     }
 }
