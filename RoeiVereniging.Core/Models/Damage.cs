621e5e38395e2ed7743f29922f9050756bb6d341
Daniel <s1213686@student.windesheim.nl>
2025-12-12 21:41:21 +0100

    feat: Damage class created

---
+++ RoeiVereniging.Core/Models/Damage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoeiVereniging.Core.Models
+{
+    public class Damage
+    {
+        public int DamageId { get; set; }
+        public int BoatId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime ReportedAt { get; set; }
+        public Damage() { }
+        public Damage(int damageId, int boatId, string description, DateTime reportedAt)
+        {
+            DamageId = damageId;
+            BoatId = boatId;
+            Description = description;
+            ReportedAt = reportedAt;
+        }
+    }
+}
