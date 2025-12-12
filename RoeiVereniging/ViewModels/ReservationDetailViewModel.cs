4de844a20baecea7a28ef0a1b2ea542adf2d953f
Daniel <s1213686@student.windesheim.nl>
2025-12-12 23:18:53 +0100

    feat: ReservationDetail page made

---
+++ RoeiVereniging/ViewModels/ReservationDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using RoeiVereniging.Core.Models;
+
+namespace RoeiVereniging.ViewModels
+{
+    public partial class ReservationDetailViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ReservationViewDTO reservation;
+        public ReservationDetailViewModel(ReservationViewDTO reservation)
+        {
+            Reservation = reservation;
+        }
+    }
+}
