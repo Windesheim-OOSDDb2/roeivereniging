f5e449a4f1e893056dcd1d10aa38f9c94ab5c038
Daniel <s1213686@student.windesheim.nl>
2025-12-13 19:16:52 +0100

    feat: Data from the boat is now showing in the reservation detail

--- RoeiVereniging/ViewModels/ReservationDetailViewModel.cs
+++ RoeiVereniging/ViewModels/ReservationDetailViewModel.cs
@@ -12,6 +12,15 @@ namespace RoeiVereniging.ViewModels
     {
         [ObservableProperty]
         private ReservationViewDTO reservation;
+
+        public string BoatName => Reservation?.BoatName ?? "";
+        public string BoatLevelText => Reservation?.BoatLevelText ?? "";
+        //public string SteeringModeText => Reservation?.SteeringModeText ?? "";  Not implemented in ReservationViewDTO
+        //public int SeatsAmount => Reservation?.SeatsAmount ?? 0; Not implemented in ReservationViewDTO
+        public DateTime StartTime => Reservation?.StartTime ?? DateTime.MinValue;
+        public DateTime EndTime => Reservation?.EndTime ?? DateTime.MinValue;
+
+
         public ReservationDetailViewModel(ReservationViewDTO reservation)
         {
             Reservation = reservation;
--- RoeiVereniging/Views/ReservationDetailView.xaml.cs
+++ RoeiVereniging/Views/ReservationDetailView.xaml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using RoeiVereniging.Core.Models;
+using RoeiVereniging.ViewModels;
 
 namespace RoeiVereniging.Views
 {
-    public partial class ReservationDetailView : ContentPage
+    public partial class ReservationDetailView : ContentPage, IQueryAttributable
     {
         public ReservationDetailView()
         {
             InitializeComponent();
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.TryGetValue("Reservation", out var reservationObj) && reservationObj is ReservationViewDTO reservation)
+            {
+                BindingContext = new ReservationDetailViewModel(reservation);
+            }
         }
     }
 }
