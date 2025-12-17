e3e7439d45d99843058e72102fa270d8858614eb
Daniel <s1213686@student.windesheim.nl>
2025-12-17 17:46:56 +0100

    feat: styled DamageHistory, made it display all damage reports.

--- RoeiVereniging/ViewModels/DamageHistoryViewModel.cs
+++ RoeiVereniging/ViewModels/DamageHistoryViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Maui.Controls;
 using RoeiVereniging.Core.Data.Repositories;
+using RoeiVereniging.Core.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace RoeiVereniging.ViewModels
 {
     [QueryProperty(nameof(BoatId), "boatId")]
     public partial class DamageHistoryViewModel : ObservableObject
     {
+        private readonly DamageRepository _damageRepository = new DamageRepository();
         private readonly BoatRepository _boatRepository = new BoatRepository();
 
         private int boatId;
@@ -19,12 +23,13 @@ namespace RoeiVereniging.ViewModels
                 {
                     var boat = _boatRepository.Get(boatId);
                     BoatName = boat?.ToString() ?? boatId.ToString();
+                    LoadDamages();
                     OnPropertyChanged(nameof(BoatDisplayText));
                 }
             }
         }
 
-        private string boatName;
+        private string boatName = string.Empty;
         public string BoatName
         {
             get => boatName;
@@ -36,5 +41,18 @@ namespace RoeiVereniging.ViewModels
         }
 
         public string BoatDisplayText => $"Historie van schade meldingen van boot: {BoatName} (ID: {BoatId})";
+
+        public ObservableCollection<Damage> Damages { get; } = new();
+
+        private void LoadDamages()
+        {
+            Damages.Clear();
+            var damages = _damageRepository.GetByBoatId(BoatId)
+                .OrderByDescending(d => d.ReportedAt);
+            foreach (var damage in damages)
+            {
+                Damages.Add(damage);
+            }
+        }
     }
 }
