830682d0bd0b922e532235ed80701e897a49d7d9
LxrdLoki <melvinVD69@outlook.com>
2025-12-18 12:28:19 +0100

    feature: found a way to pass data into component using a bindable property (for UI binding)

--- RoeiVereniging/Views/components/TableComponent.xaml.cs
+++ RoeiVereniging/Views/components/TableComponent.xaml.cs
@@ -1,3 +1,6 @@
+using Android.Widget;
+using System.Collections;
+
 namespace RoeiVereniging.Views.Components;
 
 public partial class TableComponent : ContentView
@@ -6,4 +9,23 @@ public partial class TableComponent : ContentView
 	{
 		InitializeComponent();
 	}
+
+    public static readonly BindableProperty ItemsSourceProperty =
+        BindableProperty.Create(
+            nameof(ItemsSource),
+            typeof(IEnumerable),
+            typeof(TableComponent),
+            propertyChanged: OnDataChange);
+
+    public IEnumerable ItemsSource
+    {
+        get => (IEnumerable)GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
+    }
+
+    private static void OnDataChange(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (TableComponent)bindable;
+        control.TableView.ItemsSource = (IEnumerable)newValue;
+    }
 }
