cb115afcb6b57ae9fd5b8cedf9e423abf6478e95
Daniel <s1213686@student.windesheim.nl>
2025-12-23 17:10:59 +0100

    feat: made the Boten at the top work for navigation.

--- RoeiVereniging/Views/BoatListView.xaml.cs
+++ RoeiVereniging/Views/BoatListView.xaml.cs
@@ -20,4 +20,24 @@ public partial class BoatListView : ContentPage
             vm.Refresh();
         }
     }
+
+    private async void OnBotenClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(BoatListView), true);
+    }
+
+    private void OnGebruikersClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigatie naar gebruikerspagina
+    }
+
+    private void OnReparatiesClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigatie naar reparatiespagina
+    }
+
+    private void OnOverzichtClicked(object sender, EventArgs e)
+    {
+        // TODO: Navigatie naar overzichtspagina
+    }
 }
