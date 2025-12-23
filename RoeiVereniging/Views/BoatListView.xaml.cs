231000b2bebf6113fc42ede44f8e93b28c7a6904
Daniel <s1213686@student.windesheim.nl>
2025-12-23 16:53:46 +0100

    feat: made it so boatlist refreshes

--- RoeiVereniging/Views/BoatListView.xaml.cs
+++ RoeiVereniging/Views/BoatListView.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.Controls;
+using RoeiVereniging.ViewModels;
 
 
 namespace RoeiVereniging.Views;
@@ -8,5 +9,15 @@ public partial class BoatListView : ContentPage
 	public BoatListView()
 	{
 		InitializeComponent();
+		BindingContext = new BoatListViewModel();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is BoatListViewModel vm)
+        {
+            vm.Refresh();
+        }
+    }
 }
