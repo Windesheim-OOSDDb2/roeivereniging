576e5ebe486a00ed96b105fba729f1032c7cea07
LxrdLoki <melvinVD69@outlook.com>
2025-12-24 12:43:47 +0100

    cleanup: removed unecessary old code (oops)

--- RoeiVereniging/ViewModels/admin/AdminDashboardViewModel.cs
+++ RoeiVereniging/ViewModels/admin/AdminDashboardViewModel.cs
@@ -5,24 +5,9 @@ namespace RoeiVereniging.ViewModels.admin
 {
     public partial class AdminDashboardViewModel : BaseViewModel
     {
-        private readonly GlobalViewModel _global;
-        private readonly IAuthService _auth;
-
-        public AdminDashboardViewModel(
-           GlobalViewModel global,
-           IAuthService auth)
+        public AdminDashboardViewModel()
         {
-            _global = global;
-            _auth = auth;
-
-            if (_global.currentUser == null || !_auth.IsAdmin(_global.currentUser))
-            {
-                MainThread.BeginInvokeOnMainThread(async () =>
-                {
-                    await Shell.Current.GoToAsync("//LoginView");
-                });
-                return;
-            }
+           
         } 
     }
 }
