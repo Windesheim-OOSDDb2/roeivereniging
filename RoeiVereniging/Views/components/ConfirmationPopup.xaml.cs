45c50723e05b12bc5ce6f6dd5e26e5a62c15c2c9
Chris Friemann <github@chris.friemann.nl>
2025-12-29 14:44:10 +0100

    feat: handle missing QR code in ConfirmationPopup gracefully

--- RoeiVereniging/Views/components/ConfirmationPopup.xaml.cs
+++ RoeiVereniging/Views/components/ConfirmationPopup.xaml.cs
@@ -5,13 +5,21 @@ namespace RoeiVereniging.Views.components;
 
 public partial class ConfirmationPopup : Popup
 {
-    public ConfirmationPopup(string title, string message, string footer, ImageSource? qrCodeImage)
+    public ConfirmationPopup(string title, string message, string footer, [Optional] ImageSource? qrCodeImage)
     {
         InitializeComponent();
         MessageTitle.Text = title;
         MessageLabel.Text = message;
         MessageFooter.Text = footer;
-        QrCodeImage.Source = qrCodeImage;
+        if (qrCodeImage == null)
+        {
+            QrCodeImage.Source = "noimage.png";
+            QrCode.IsVisible = false;
+            QrCodeImage.IsVisible = false;
+        } else
+        {
+            QrCodeImage.Source = qrCodeImage;
+        }
     }
 
     private void OnDoneClicked(object sender, EventArgs e)
