674a52a1648219a2e92d45fdb3c92b73031a5429
Daniel <s1213686@student.windesheim.nl>
2026-01-04 01:11:08 +0100

    feat: changed boatlist header to the admin component

--- RoeiVereniging/Views/BoatListView.xaml.cs
+++ RoeiVereniging/Views/BoatListView.xaml.cs
@@ -7,11 +7,11 @@ namespace RoeiVereniging.Views;
 
 public partial class BoatListView : ContentPage
 {
-	public BoatListView()
-	{
-		InitializeComponent();
-		BindingContext = new BoatListViewModel();
-	}
+    public BoatListView()
+    {
+        InitializeComponent();
+        BindingContext = new BoatListViewModel();
+    }
 
     protected override void OnAppearing()
     {
@@ -20,25 +20,5 @@ public partial class BoatListView : ContentPage
         {
             vm.Refresh();
         }
-    }
-
-    private async void OnBotenClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(BoatListView), true);
-    }
-
-    private async void OnGebruikersClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(UserView), true);
-    }
-
-    private void OnReparatiesClicked(object sender, EventArgs e)
-    {
-        // TODO: Navigatie naar reparatiespagina
-    }
-
-    private async void OnOverzichtClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(AdminDashboardView), true);
     }
 }
